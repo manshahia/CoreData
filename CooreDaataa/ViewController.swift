d62f4069ee5ab6b8a75c7343f7f04916934e1ba4
Ravi Inder Manshahia <ravinder@lab91.co>
2018-11-16 10:13:53 +0530

    Insert and Fetch Data in Core Data

--- CooreDaataa/ViewController.swift
+++ CooreDaataa/ViewController.swift
@@ -7,19 +7,60 @@
 //
 
 import UIKit
-
+import CoreData
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
+        
+        //SAVING DATA
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let User = NSEntityDescription.insertNewObject(forEntityName: "Users", into: context)
+        User.setValue("Tanisah", forKey: "username")
+        User.setValue("psdfgss", forKey: "password")
+        User.setValue(32, forKey: "age")
+        
+        do
+        {
+            try context.save()
+            print("Values asaved")
+            
+        } catch {
+            print("Error while saving")
+        }
+        
+        //Fetching DATA
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Users")
+        request.returnsObjectsAsFaults = false
+        
+        do
+        {
+            let results = try context.fetch(request)
+            if results.count > 0
+            {
+                for result in results as! [NSManagedObject]
+                {
+                    if let username = result.value(forKey: "username") as? String
+                    {
+                        print(username)
+                    }
+                }
+            }
+        } catch {
+            print("Error occured while fetching the results")
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+ 
+        override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+            // Dispose of any resources that can be recreated.
+        }
+        
+        
 }
 
